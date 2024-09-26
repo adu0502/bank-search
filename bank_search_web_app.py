2d759cf9247a2d6189c7c6a7ad0813225c41e133
Aditi Bhatt <124800243+adu0502@users.noreply.github.com>
2024-09-26 08:06:27 +0530

    Update bank_search_web_app.py

--- bank_search_web_app.py
+++ bank_search_web_app.py
@@ -156,8 +156,8 @@ st.header("IDFC First Bank Website Search", divider=True)
 # openai.api_key = os.getenv('GPT_KEY')
 # client = openai.OpenAI(api_key = api_key_input)
 
-openai.api_key = OPENAI_API_KEY
-client = openai.OpenAI(api_key = OPENAI_API_KEY)
+openai.api_key = st.secrets['OPENAI_API_KEY']
+client = openai.OpenAI(api_key = st.secrets[['OPENAI_API_KEY'])
 
 # Initialize OpenAI client with the provided API key
 # if api_key_input:
