cca706a912f3d8818a30c71d7395679f84746478
Aditi Bhatt <124800243+adu0502@users.noreply.github.com>
2024-09-26 12:28:24 +0530

    Update bank_search_web_app.py

--- bank_search_web_app.py
+++ bank_search_web_app.py
@@ -185,25 +185,6 @@ User Query:
 
 st.set_page_config(page_title="IDFC First Bank Search", page_icon="🔍")
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
-
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 left_co, cent_co,last_co = st.columns(3)
 with cent_co:
